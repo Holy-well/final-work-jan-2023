25cd95391348c1568f3015b4ccde3a60a2273591
Holy-well <116588100+Holy-well@users.noreply.github.com>
2023-02-01 16:08:41 +0300

    Add all files
    Добавил все файлы вручную, так как при привязки репозитория не указал в конце ссылки .git

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] a;
+int count = 0; 
+string s;
+
+Console.Write("count = ");
+count = Int32.Parse(Console.ReadLine());
+
+a = new string[count];
+Console.WriteLine();
+
+for (int i=0; i<a.Length; i++)
+    {
+    Console.Write("a[{0}] = ", i);
+    a[i] = Console.ReadLine();
+    }
+
+int stringsize = 4;
+
+for (int i=0; i<a.Length; i++)
+    {
+    int strlength = a[i].Length;
+    if (a[i].Length < stringsize) 
+        Console.WriteLine (a[i]);
+    }
+
